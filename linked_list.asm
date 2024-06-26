9d81f28573621526b3bad51fbf35b5a538c90ecd
Candid Moe <candidmoe@gmail.com>
2024-06-26 09:43:37 -0400

    linked list

---
+++ linked_list.asm
@@ -0,0 +1,34 @@
+; linked_list.asm - (c) Candid Moe - 2024
+;
+; Collection of generic linked list routines
+;
+
+public linked_list_add
+
+linked_list_add:
+    ;   Add a new entry at the linked list start
+    ;   
+    ;   Stack parameters:
+    ;   - Address of new entry
+    ;   - Address of pointer to list.
+    ;    
+    exx
+    push IY
+
+    ld  IY, 0
+    add IY, sp
+
+    ld  DE, (iy + 4);   @ new entry
+    ld  HL, (iy + 6);   @ list head
+    ld  BC, 2
+    ldir            ;   new entry -> previous entry
+    ;
+    ld  DE, (iy + 4);    @ new entry
+    ld  HL, (iy + 6);   @ list head
+    ld  (HL), DE    ;   list head -> new entry
+    
+    pop IY
+    exx
+
+    ret
+    
