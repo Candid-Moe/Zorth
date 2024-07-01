33e06043f08164739bdc866f6365cb8026edb497
Candid Moe <candidmoe@gmail.com>
2024-07-01 07:33:48 -0400

    Add documentation directory

--- storage.asm
+++ storage.asm
@@ -2,8 +2,8 @@ TRUE:       dw -1
 FALSE:      dw  0
 _SOURCE_ID: db   DEV_STDIN     ; 1 = keyboard
 _gTIB:       db  8        ; #TIB, len of string in _TIB
-;_TIB:      defs 80     ; Input line
-_TIB:       defs 80, " una dos"
+_TIB:      defs 80     ; Input line
+;_TIB:       defs 80, " una dos"
 _gtIN:      dw   0      ; >IN, Index into TIB
 _PAD:       defs 80
 _PROMPT:    counted_string   ">"
